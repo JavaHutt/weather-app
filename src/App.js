b3f5fcaaf177161e18b497b812dc086d6bde34c9
unknown <alxmer111@gmail.com>
2019-01-09 17:23:30 +0400

    paragraphs with data

--- src/App.js
+++ src/App.js
@@ -20,15 +20,26 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ''
-    })
+    if ( city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the values'
+      })
+    }    
   }
   render() {
     return(
--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component {
     render() {
         return(
             <div>
-                { this.props.temperature }
+                { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
+                { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+                { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
+                { this.props.description && <p>Condition: { this.props.description }</p> }
+                { this.props.error && <p> { this.props.error }</p>}
             </div>
         );
     }
